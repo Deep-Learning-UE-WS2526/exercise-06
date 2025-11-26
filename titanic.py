9cd34f79c2e741afd9593bfe942d989c8e14e4af
mar0303 <scherfmarilis@gmail.com>
2025-11-26 21:56:15 +0100

    erster Versuch

--- titanic.py
+++ titanic.py
@@ -1,7 +1,12 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
+from keras import layers
+from sklearn.metrics import precision_score, recall_score, f1_score
 
 # read the data from a CSV file (included in the repository)
-df = pd.read_csv("data/train.csv")
+df = pd.read_csv("C:/Users/maris/Documents/Uni/IV/Deep Learning/exercise-06/data/train.csv")
 
 # ## Step 3
 # 1. Remove the columns "Name" and "PassengerId" (because we know they are irrelevant for our problem).
@@ -27,22 +32,65 @@ x = df.drop("Survived", axis=1)
 
 # 2. Secondly, we need to split training and test data. This can be done with the function [`sklearn.model_selection.train_test_split()`](https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html#sklearn.model_selection.train_test_split) from the `scikit-learn` library.
 
-from sklearn.model_selection import train_test_split
-
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0, test_size=0.1)
 
-# 3. Finally, initialize a LogisticRegression object with a `liblinear` solver, and fit it to the training data.
-from sklearn.linear_model import LogisticRegression
+# Set up a network with a single hidden layer of size 10. 
+# The hidden and the output layer use the softmax activation function. 
+# Test and evaluate.
 
-classifier = LogisticRegression(random_state=0, solver="liblinear")
-classifier.fit(x_train, y_train)
+# model = keras.Sequential()
+# model.add(layers.Input(shape=(9,)))
+# model.add(layers.Dense(10, activation = "softmax"))
+# model.add(layers.Dense(2, activation = "softmax"))
 
-# 4. Lastly, calculate precision/recall/f-score on the test data using the appropriate functions from `scikit-learn`.
-y_pred = classifier.predict(x_test)
+# model.compile(loss="mean_squared_error", optimizer="sgd", metrics=["accuracy"])
 
-from sklearn.metrics import precision_score, recall_score, f1_score
+# model.fit(x_train, y_train, epochs=50, batch_size=5)
+
+# model.evaluate(x_test, y_test, batch_size=20)
+
+# Create a network with 2 hidden layers of sizes 20 and 10. 
+# The first layer uses a sigmoid activation, 
+# the second one relu (output layer should use softmax again).
+
+model = keras.Sequential()
+model.add(layers.Input(shape=(9,)))
+model.add(layers.Dense(20, activation = "sigmoid"))
+model.add(layers.Dense(10, activation = "relu"))
+model.add(layers.Dense(2, activation = "softmax"))
+
+model.compile(loss="mean_squared_error", optimizer="sgd", metrics=["accuracy"]) 
+
+model.fit(x_train, y_train, epochs=50, batch_size=5)
+model.evaluate(x_test, y_test, batch_size=20)
+
+y_pred = np.argmax(model.predict(x_test), axis=1)
+
+
 
 print("precision: "+ str(precision_score(y_test, y_pred)))
 print("recall: "+ str(recall_score(y_test, y_pred)))
 print("f1: "+ str(f1_score(y_test, y_pred)))
 
+
+
+
+
+
+
+
+
+# # 3. Finally, initialize a LogisticRegression object with a `liblinear` solver, and fit it to the training data.
+# from sklearn.linear_model import LogisticRegression
+
+# classifier = LogisticRegression(random_state=0, solver="liblinear")
+# classifier.fit(x_train, y_train)
+
+# # 4. Lastly, calculate precision/recall/f-score on the test data using the appropriate functions from `scikit-learn`.
+# y_pred = classifier.predict(x_test)
+
+# from sklearn.metrics import precision_score, recall_score, f1_score
+
+# print("precision: "+ str(precision_score(y_test, y_pred)))
+# print("recall: "+ str(recall_score(y_test, y_pred)))
+# print("f1: "+ str(f1_score(y_test, y_pred)))
